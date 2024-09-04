fd1e1d76cbbf47a80446beae3a656802bd1cf84b
seantvpg <sd816323@ohio.edu>
2024-09-04 13:31:54 -0400

    final

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sean Dabydeen
+ *        Date: 09\04\2024
+ * Description: git-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main (int argc, char const *argv[])
+
+{
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
